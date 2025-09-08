57a8ce7780b88a7188fb337a1087637ddcb7c50a
Gupta <u435755@global.ual.com>
2025-09-08 12:09:51 +0530

    Initial Commit

---
+++ src/main/java/com/ual/JenkinsLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsLearningApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/ual/JenkinsLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
